e1df54314bf6d24ce12ec2bd56db06c06d4bfcae
Kimgooner <nubuli19@gmail.com>
2026-07-15 16:30:54 +0900

    feat: jenkins init

---
+++ jenkins/seed-job.groovy
@@ -0,0 +1,9 @@
+folder('stage') {
+    displayName('stage')
+    description('Jobs that deploy to the stage environment.')
+}
+
+folder('operation') {
+    displayName('operation')
+    description('Jobs that deploy to the operation environment.')
+}
